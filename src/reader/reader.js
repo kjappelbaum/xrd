d0b601c81216fd4ef0e0b3d43eafebab77b142f2
Kevin <kevin.jablonka@epfl.ch>
2020-06-29 22:44:28 +0200

    feat: implemented xyy parser

--- src/reader/reader.js
+++ src/reader/reader.js
@@ -1,6 +1,6 @@
 import JSZip from 'jszip/dist/jszip.min.js';
 
-import { parseDiffractogram } from '../parser/parser';
+import { parseDiffractogram, parseXY } from '../parser/parser';
 
 /**
  * @param  {} binary
@@ -9,11 +9,16 @@ import { parseDiffractogram } from '../parser/parser';
 // eslint-disable-next-line no-unused-vars
 export async function readBRML(binary, options = {}) {
   let zip = new JSZip();
-  const txt = await zip.loadAsync(binary).then(function (zipFiles) {
+  const txt = await zip.loadAsync(binary).then(function(zipFiles) {
     return zipFiles.file('Experiment0/RawData0.xml').async('text');
   });
 
   const diffractogram = parseDiffractogram(txt);
 
+  return diffractogram;
+}
+
+export function readXY(file) {
+  const diffractogram = parseXY(file);
   return diffractogram;
 }
